bae00472a12462d93f72039ff5a66c57cbfac55a
ant1234 <antonyvidetta1986@gmail.com>
2024-09-08 22:07:52 +1200

    Add update functions

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -31,6 +31,15 @@ class NoteController extends Controller
     public function store(Request $request)
     {
         //
+        $data = $request->validate([
+            'note' => ['required', 'string']
+        ]);
+
+        $data['user_id'] = 1;
+        $note = Note::create($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was created!');
+    
     }
 
     /**
@@ -55,6 +64,15 @@ class NoteController extends Controller
     public function update(Request $request, Note $note)
     {
         //
+
+        $data = $request->validate([
+            'note' => ['required', 'string']
+        ]);
+
+        $note->update($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was updated!');
+    
     }
 
     /**
